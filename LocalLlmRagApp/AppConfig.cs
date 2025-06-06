2b65e617789e2236767e868b84a57c6ea9e6230c
suusanex <suusan@himado.net>
2025-06-06 11:26:10 +0900

    コンソールアプリの骨組みを手動作成

---
+++ LocalLlmRagApp/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLlmRagApp;
+
+public class AppConfig
+{
+    /// <summary>
+    /// UserSecretから読み込む
+    /// </summary>
+    public string Secret1 { get; set; }
+
+    /// <summary>
+    /// appsettings.jsonから読み込む
+    /// </summary>
+    public string Other1 { get; set; }
+
+}
