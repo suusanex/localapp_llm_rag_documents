bd04027a046ffa965ac5af17b9a3100e0e781028
suusanex <suusan@himado.net>
2025-06-06 11:26:10 +0900

    コンソールアプリの骨組みを手動作成

---
+++ LocalLlmRagApp/Program.cs
@@ -0,0 +1,35 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Diagnostics;
+using LocalLlmRagApp;
+
+_ = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging((context, logging) =>
+    {
+        logging.ClearProviders();
+        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    })
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets<Program>(optional: true, reloadOnChange: true);
+    })
+    .ConfigureServices(ConfigureServices)
+    .RunConsoleAsync();
+
+
+void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+{
+    services.AddHostedService<ConsoleHostedService>();
+    services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+
+}
